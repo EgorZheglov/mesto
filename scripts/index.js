a7526687c26b83cdf0a41852d5c41ccb6f52e8db
Egor Zheglov <egorzh97@yandex.ru>
2021-04-22 15:52:59 +0300

    fonts, input, close button fix

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ let closePopupButton = document.querySelector('.popup__close');
 // Находим форму в DOM
 let formElement = document.querySelector('.popup__form');// Воспользуйтесь методом querySelector()
 // Находим поля формы в DOM
-let nameInput = document.querySelector('.popup__name-input');// Воспользуйтесь инструментом .querySelector()
-let jobInput = document.querySelector('.popup__profession-input');// Воспользуйтесь инструментом .querySelector()
+let nameInput = document.querySelector('#name_input');// Воспользуйтесь инструментом .querySelector()
+let jobInput = document.querySelector('#profession_input');// Воспользуйтесь инструментом .querySelector()
 
 let profileName = document.querySelector('.profile__name');
 let profileProfession = document.querySelector('.profile__profession');
