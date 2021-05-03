5ab7d524a68caff620903697216b9fb2671a9cc6
Egor Zheglov <egorzh97@yandex.ru>
2021-05-03 16:45:22 +0300

    fix conflict

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-=======
-const openPopupButton = document.querySelector('.profile__edit-button');
-const popup = document.querySelector('.popup');
-const closePopupButton = document.querySelector('.popup__close');
-// Находим форму в DOM
-let formElement = document.querySelector('.popup__form');// Воспользуйтесь методом querySelector()
-// Находим поля формы в DOM
-let nameInput = document.querySelector('.popup__name-input');// Воспользуйтесь инструментом .querySelector()
-let jobInput = document.querySelector('.popup__profession-input');// Воспользуйтесь инструментом .querySelector()
->>>>>>> parent of 2aa352c (Dynamic content add)
-
 let openPopupButtonEdit = document.querySelector('.profile__edit-button');
 let openPopupButtonAdd = document.querySelector('.profile__add-button');
 let popupEdit = document.querySelector('.popup-edit');
@@ -22,20 +10,6 @@ let jobInput = document.querySelector('#profession_input');
 let profileName = document.querySelector('.profile__name');
 let profileProfession = document.querySelector('.profile__profession');
 
-<<<<<<< HEAD
-=======
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-
-
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-
-
-nameInput.textContent = profileName.value;
-jobInput.textContent = profileProfession.value;
->>>>>>> parent of 2aa352c (Dynamic content add)
 
 function openPopupEdit(evt){
     nameInput.value = profileName.textContent;
@@ -52,19 +26,16 @@ function openPopupAdd(evt){
     popupAdd.classList.add('popup_is-opened');
 }
 
-function togglePopup(event){
-    event.preventDefault();
-    popup.classList.toggle('popup_is-opened');
-}
 
 function closePopupAdd(evt){
     popupAdd.classList.remove('popup_is-opened');
 }
 
 function submitFormHandler (evt) {
+    evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileProfession.textContent = jobInput.value;
-    closePopup();
+    closePopupEdit();
 }
 
 openPopupButtonEdit.addEventListener("click", openPopupEdit);
