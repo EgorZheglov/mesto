97f6214bec9c9744b4f06bc883cd02f85e43379c
Egor Zheglov <egorzh97@yandex.ru>
2021-05-07 02:48:06 +0300

    hotfixfinal

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-
+const popup = document.querySelector('.popup');
 const openPopupButtonEdit = document.querySelector('.profile__edit-button');
 const openPopupButtonAdd = document.querySelector('.profile__add-button');
 const popupEdit = document.querySelector('.popup_type_edit');
@@ -24,6 +24,7 @@ const popupPhoto = document.querySelector('.popup_type_photo');
 
 //Странно, что при изменении атрибута переменной, которая объявлена константой вроде 
 //textContent или value - не возникает ошибки и все работает. Получается, атрибуты const не являются постоянными? 
+
   function createNewCard(name,link){
 
     let newCard = cardTemplate.content.querySelector('.elements__item').cloneNode(true);
@@ -32,30 +33,25 @@ const popupPhoto = document.querySelector('.popup_type_photo');
     let cardPopup = newCard.querySelector('.elements__popup-button');
 
     cardPopup.addEventListener("click", function(event){
+      openPopup(popupPhoto);
       popupPhoto.querySelector('.popup__photo').src=link;
-      popupPhoto.querySelector('.popup__photo-title').textContent=name;
-      popupPhoto.classList.remove('popup_type_photo');/*В данном случае, почему-то недостаточно просто добавить модификатор _is-opened*/
-      popupPhoto.classList.add('popup_is-opened');
-
-      popupPhoto.querySelector('.popup__close_photo').addEventListener("click", function(event){
-        popupPhoto.classList.add('popup_type_photo');
-        popupPhoto.classList.remove('popup_is-opened');
-      })
+      popupPhoto.querySelector('.popup__photo').alt = name + '';
+      popupPhoto.querySelector('.popup__photo-title').textContent = name;
   });
-
-    photoLikeButton.addEventListener("click", function(event){
+    photoLikeButton.addEventListener('click', function(event){
       photoLikeButton.classList.toggle('elements__like-button_active');
   });
-
-    deleteButton.addEventListener("click", function(event){
+    deleteButton.addEventListener('click', function(event){
         event.target.closest('.elements__item').remove();
     });
-
     newCard.querySelector('.elements__title').textContent = name;
     newCard.querySelector('.elements__photo').src = link;
+    newCard.querySelector('.elements__photo').alt= name + '';
 
     return newCard;
   }
+
+  console.log(popupPhoto.querySelector('.popup__photo').alt);
 
   for(let i = 0; i < initialCards.length; i += 1){
         let currentItem = initialCards[i];
@@ -63,52 +59,50 @@ const popupPhoto = document.querySelector('.popup_type_photo');
   }
 
 
-
-function openPopupEdit(evt){
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileProfession.textContent;
-    popupEdit.classList.add('popup_is-opened');
+function openPopup(popup){
+  popup.classList.add('popup_is-opened');
 }
 
-function closePopupEdit(evt){
-    popupEdit.classList.remove('popup_is-opened');
-}
-
-
-function openPopupAdd(evt){
-    popupAdd.classList.add('popup_is-opened');
-}
-
-
-function closePopupAdd(evt){
-    popupAdd.classList.remove('popup_is-opened');
+function closePopup(popup){
+    popup.classList.remove('popup_is-opened');
 }
 
 function submitFormHandlerEdit (evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileProfession.textContent = jobInput.value;
-    closePopupEdit();
+    closePopup(popupEdit);;
 }
-
 
 function submitFormHandlerAdd (evt) {
-    evt.preventDefault();
-    
+    evt.preventDefault();  
     //initialCards.unshift({name:photoNameInput.value, link:photoLinkInput});
     cardContainer.prepend(createNewCard(photoNameInput.value,photoLinkInput.value));
-    closePopupAdd();
+    formAddElement.reset();
+    closePopup(popupAdd);
 }
 
+openPopupButtonEdit.addEventListener('click', function (event){
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileProfession.textContent;
+  openPopup(popupEdit);
+});
 
+openPopupButtonAdd.addEventListener('click', function (event){
+  openPopup(popupAdd);
+});
 
-openPopupButtonEdit.addEventListener("click", openPopupEdit);
+closePopupAddButton.addEventListener('click', function (event){
+  closePopup(popupAdd);
+});
 
-openPopupButtonAdd.addEventListener("click", openPopupAdd);
+closePopupEditButton.addEventListener('click', function (event){
+  closePopup(popupEdit);
+});
 
-closePopupAddButton.addEventListener("click", closePopupAdd);
-
-closePopupEditButton.addEventListener("click", closePopupEdit);
+popupPhoto.querySelector('.popup__close_photo').addEventListener("click", function(event){
+  closePopup(popupPhoto);
+});
 
 formEditElement.addEventListener('submit', submitFormHandlerEdit); 
 
