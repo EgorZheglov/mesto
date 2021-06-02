6e38d2c2f14771e19347dfef35ec0610fba4281d
Egor Zheglov <egorzh97@yandex.ru>
2021-06-03 00:16:09 +0300

    hotfix console error GH-pages 4

--- utils/utils.js
+++ utils/utils.js
@@ -27,4 +27,4 @@ function closePopup(popup) {
     popup.classList.remove("popup_is-opened");
   }
 
-export {openPopup, popupPhoto, closePopup}
+export {openPopup, closeByEsc, closeByOverlay, popupPhoto, closePopup} 
