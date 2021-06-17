ebcc623aa1da20f71bda5d746429c2d8b4d38e17
Egor Zheglov <egorzh97@yandex.ru>
2021-06-17 13:27:23 +0300

    develop final. bugfix

--- scripts/PopupWithForm.js
+++ scripts/PopupWithForm.js
@@ -10,16 +10,17 @@ export class PopupWithForm extends Popup{
     setEventListeners(){
         super.setEventListeners();
 
-        this._inputValues = this._getInputValues();
-
         this._popup
         .querySelector('.popup__form')
-        .addEventListener('submit', (evt) => {
-            evt.preventDefault();
-
-            this._submitFunction(this._inputValues); 
-        })
+        .addEventListener('submit', this._submitFormHandeler);
     }
+
+    _submitFormHandeler = (evt) =>{
+        evt.preventDefault();
+
+        this._submitFunction(this._getInputValues()) 
+    }
+
 
     _getInputValues(){
         return Array.from(this._popup.querySelectorAll('.popup__input'));
@@ -27,6 +28,11 @@ export class PopupWithForm extends Popup{
 
     closePopup(){
         super.closePopup();
+
+        this._popup
+        .querySelector('.popup__form')
+        .removeEventListener('submit', this._submitFormHandeler);
+
         this._popup
         .querySelector('.popup__form')
         .reset();
